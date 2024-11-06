065f37b48f3ca92b87c5f7403febd0cea86cfd8d
Princess Pocon <elinepocon@gmail.com>
2024-11-06 16:51:02 +0800

    docs: add comments on code

--- server.js
+++ server.js
@@ -9,12 +9,21 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
+/*
+The default route (home page) when you visit the root URL (/). It renders the index.ejs view.
+The meme: null part is passed to the view to indicate that no meme is displayed initially when the user first visits the page.
+*/
 app.get("/", (req, res) => {
   res.render("index", {
     meme: null,
   });
 });
 
+/*
+This route is used to fetch a random meme from the Imgflip API.
+The axios.get() method makes a GET request to the Imgflip API at https://api.imgflip.com/get_memes to retrieve a list of memes.
+If an error occurs while fetching the meme data, an error page (error.ejs) is rendered, passing the error information to the view. This is helpful for debugging or showing an error message to the user.
+*/
 app.get("/meme", (req, res) => {
   axios
     .get("https://api.imgflip.com/get_memes")
