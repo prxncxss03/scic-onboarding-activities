d3677fe0ecaad0c0a669f7023da6746d4831e394
Princess Pocon <elinepocon@gmail.com>
2024-11-07 16:32:15 +0800

    fix: make generate button width to 100%; fix: change console.log on port

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ app.get("/meme", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
